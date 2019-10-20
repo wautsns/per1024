c1a73b6c1fa5d98e01228c888213b3ff539fa203
wautsns <wautsns@foxmail.com>
2019-10-20 15:28:42 +0800

    style(microserv-auth): 格式上的调整

--- per1024-microserv/per1024-microserv-auth/src/main/java/com/github/wautsns/project/per1024/auth/MicroservAuthApplication.java
+++ per1024-microserv/per1024-microserv-auth/src/main/java/com/github/wautsns/project/per1024/auth/MicroservAuthApplication.java
@@ -8,18 +8,18 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
-*
-* @author wautsns
-* @version 0.1.0 Aug 23, 2019
-*/
+ *
+ * @author wautsns
+ * @version 0.1.0 Aug 23, 2019
+ */
 @EnableDiscoveryClient
 @EnableDubbo(scanBasePackages = "com.github.wautsns.project.per1024.auth.business.api.rpc")
 @MapperScan("com.github.wautsns.project.per1024.auth.business.repository.mapper")
 @SpringBootApplication
 public class MicroservAuthApplication {
 
-   public static void main(String[] args) throws Exception {
-       SpringApplication.run(MicroservAuthApplication.class, args);
-   }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MicroservAuthApplication.class, args);
+    }
 
 }
